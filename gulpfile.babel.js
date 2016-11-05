f2372c327091a6e3eeb754dceb24c79862d237c8
Tetsutaro Endo <tetsu9taro25@gmail.com>
2016-11-06 01:30:08 +0900

    Apply webpack to gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,11 +4,15 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
-import { exec } from 'child_process';
 import eslint from 'gulp-eslint';
+import webpack from 'webpack-stream';
+import webpackConfig from './webpack.config.babel';
 
 const paths = {
-  allSrcJs: 'src/**/*.js',
+  allSrcJs: 'src/**/*.js?(x)',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.js',
   gulpFile: 'gulpfile.babel.js',
   webpackFile: 'webpack.config.babel.js',
   libDir: 'lib',
@@ -19,6 +23,7 @@ gulp.task('lint', () =>
   gulp.src([
     paths.allSrcJs,
     paths.gulpFile,
+    paths.webpackFile,
   ])
   .pipe(eslint())
   .pipe(eslint.format())
@@ -35,12 +40,11 @@ gulp.task('build', ['lint', 'clean'], () =>
     .pipe(gulp.dest(paths.libDir))
 );
 
-gulp.task('main', ['build'], (callback) => {
-  exec(`node ${paths.libDir}/server`, (error, stdout) => {
-    console.log(stdout);
-    return callback(error);
-  });
-});
+gulp.task('main', ['lint', 'clean'], () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir))
+);
 
 gulp.task('watch', () =>
   gulp.watch([paths.allSrcJs, paths.gulpFile], ['main'])
